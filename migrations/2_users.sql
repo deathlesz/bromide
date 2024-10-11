d5ed5b017b10edaaadd76557e0a770b25232fabf
Deathlesz <deathless.mcd@gmail.com>
2024-10-11 09:38:42 +0300

    feat: use better data types in database

--- migrations/2_users.sql
+++ migrations/2_users.sql
@@ -1,43 +1,45 @@
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    account_id INT UNIQUE NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT UNIQUE NOT NULL,
 
-    icon_id INT NOT NULL DEFAULT 1,
-    icon_type INT NOT NULL DEFAULT 0,
-    glowing INT NOT NULL DEFAULT 0,
-    primary_color INT NOT NULL DEFAULT 0,
-    secondary_color INT NOT NULL DEFAULT 3,
-    tertiary_color INT NOT NULL DEFAULT 0,
-    cube_id INT NOT NULL DEFAULT 1,
-    ship_id INT NOT NULL DEFAULT 1,
-    ball_id INT NOT NULL DEFAULT 1,
-    ufo_id INT NOT NULL DEFAULT 1,
-    wave_id INT NOT NULL DEFAULT 1,
-    robot_id INT NOT NULL DEFAULT 1,
-    spider_id INT NOT NULL DEFAULT 1,
-    swing_id INT NOT NULL DEFAULT 1,
-    jetpack_id INT NOT NULL DEFAULT 1,
-    glow_id INT NOT NULL DEFAULT 0,
-    explosion_id INT NOT NULL DEFAULT 1,
+    icon_id SMALLINT NOT NULL DEFAULT 1,
+    icon_type SMALLINT NOT NULL DEFAULT 0,
+    glowing BOOLEAN NOT NULL DEFAULT FALSE,
+    primary_color SMALLINT NOT NULL DEFAULT 0,
+    secondary_color SMALLINT NOT NULL DEFAULT 3,
+    tertiary_color SMALLINT NOT NULL DEFAULT 0,
+    cube_id SMALLINT NOT NULL DEFAULT 1,
+    ship_id SMALLINT NOT NULL DEFAULT 1,
+    ball_id SMALLINT NOT NULL DEFAULT 1,
+    ufo_id SMALLINT NOT NULL DEFAULT 1,
+    wave_id SMALLINT NOT NULL DEFAULT 1,
+    robot_id SMALLINT NOT NULL DEFAULT 1,
+    spider_id SMALLINT NOT NULL DEFAULT 1,
+    swing_id SMALLINT NOT NULL DEFAULT 1,
+    jetpack_id SMALLINT NOT NULL DEFAULT 1,
+    glow_id SMALLINT NOT NULL DEFAULT 0,
+    explosion_id SMALLINT NOT NULL DEFAULT 1,
 
-    stars INT NOT NULL DEFAULT 0,
-    moons INT NOT NULL DEFAULT 0,
-    diamonds INT NOT NULL DEFAULT 0,
-    secret_coins INT NOT NULL DEFAULT 0,
-    user_coins INT NOT NULL DEFAULT 0,
-    creator_points INT NOT NULL DEFAULT 0,
+    stars BIGINT NOT NULL DEFAULT 0,
+    moons BIGINT NOT NULL DEFAULT 0,
+    diamonds BIGINT NOT NULL DEFAULT 0,
+    secret_coins BIGINT NOT NULL DEFAULT 0,
+    user_coins BIGINT NOT NULL DEFAULT 0,
+    creator_points BIGINT NOT NULL DEFAULT 0,
 
-    message_state INT NOT NULL DEFAULT 0,
-    friend_state INT NOT NULL DEFAULT 0,
-    comment_history_state INT NOT NULL DEFAULT 0,
+    -- TODO: use enums?
+    message_state SMALLINT NOT NULL DEFAULT 0,
+    friend_state SMALLINT NOT NULL DEFAULT 0,
+    comment_history_state SMALLINT NOT NULL DEFAULT 0,
 
-    youtube_url TEXT NOT NULL DEFAULT '',
-    twitter_url TEXT NOT NULL DEFAULT '',
-    twitch_url TEXT NOT NULL DEFAULT '',
+    youtube_url VARCHAR(255) NOT NULL DEFAULT '',
+    twitter_url VARCHAR(255) NOT NULL DEFAULT '',
+    twitch_url VARCHAR(255) NOT NULL DEFAULT '',
 
-    mod_level INT NOT NULL DEFAULT 0,
+    -- TODO: here too?
+    mod_level SMALLINT NOT NULL DEFAULT 0,
 
-    CONSTRAINT users_account_id_fkey FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE
+    FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE
 )
